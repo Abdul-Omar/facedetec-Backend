f0c270f0732397386f15ce407e24eaa19016cb0c
Abdul Omar <aahmed@ucsd.edu>
2019-11-18 21:42:35 -0800

    working now

--- controllers/image.js
+++ controllers/image.js
@@ -2,9 +2,10 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '164021376927458bb3aa848036b037a6'
+ apiKey: '9042cf1bd077404e85141857f2cef3b6'
 });
-
+   
+      
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    user : 'postgres',
+    password : '334455qq',
+    database : 'facedetect'
   }
 });
 
